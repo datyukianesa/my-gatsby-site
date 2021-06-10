bd484cf513e7bddf9a4cfc364d3ebfd512f86e7c
datyukianesa <funkyfanni233@gmail.com>
2021-06-10 20:06:46 +0700

    finished tutorial 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
 	return (
 		<Layout pageTitle='Home Page'>
 			<p>I'm making this by following the Gatsby Tutorial.'</p>
+			<StaticImage
+				alt='Hoshimachi Suisei - Ghost'
+				src='../images/img.jpg'></StaticImage>
 		</Layout>
 	)
 }
