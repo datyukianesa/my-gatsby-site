b12e05e74784d9bf75810746899da660252a8bd4
datyukianesa <funkyfanni233@gmail.com>
2021-06-10 19:41:26 +0700

    added plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  siteMetadata: {
-    title: "My Gatsby Site",
-  },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-react-helmet"],
-};
+	siteMetadata: {
+		title: 'My Gatsby Site',
+	},
+	plugins: [
+		'gatsby-plugin-netlify-cms',
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-image',
+		'gatsby-plugin-sharp',
+	],
+}
